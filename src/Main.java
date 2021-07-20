1eaa8882c201b48076ff67c1c67aefadb84ddae4
AAB-87 <andrej-abrekov@yandex.ru>
2021-07-20 20:49:01 +0400

    added CamelCase and fixed result freeMiles

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double ticket_price = 357.7;
-        int one_mile = 20;
-        double total_miles = ticket_price/one_mile;
-        double free_miles = (int) total_miles;
-        System.out.println(free_miles);
+        double ticketPrice = 373.8;
+        int oneMile = 20;
+        int freeMiles = (int) (ticketPrice/oneMile);
+        System.out.println(freeMiles);
     }
 }
